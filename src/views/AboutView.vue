349da56c31b41c471e6e02d56a4242c9326f6e9c
Norman <norman-rdd@mail.ru>
2025-05-13 19:30:49 +0600

    commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div>
+      <button @click="increment">clicker</button>
+    </div>
+    <p>{{ count }}</p>
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const increment = ()=> {
+  count.value++
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
